b1a6956953fec303f3877806c67530d8e793ef5c
Akash Idnani <aidnanidev@gmail.com>
2020-03-11 22:37:56 -0700

    Structuring project

--- Core/Inc/main.h
+++ Core/Inc/main.h
@@ -4,6 +4,7 @@
 #include "stm32f1xx_hal.h"
 
 void Error_Handler(void);
+void SystemClock_Config(void);
 
 #define LED_Pin GPIO_PIN_13
 #define LED_GPIO_Port GPIOC
--- Core/Src/main.c
+++ Core/Src/main.c
@@ -8,7 +8,6 @@ const osThreadAttr_t defaultTask_attributes = {
         .stack_size = 128 * 4
 };
 
-void SystemClock_Config(void);
 
 static void MX_GPIO_Init(void);
 
@@ -29,35 +28,6 @@ int main(void) {
     while (1) {}
 }
 
-void SystemClock_Config(void) {
-    RCC_OscInitTypeDef RCC_OscInitStruct = {0};
-    RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
-
-    /** Initializes the CPU, AHB and APB busses clocks
-    */
-    RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
-    RCC_OscInitStruct.HSEState = RCC_HSE_ON;
-    RCC_OscInitStruct.HSEPredivValue = RCC_HSE_PREDIV_DIV1;
-    RCC_OscInitStruct.HSIState = RCC_HSI_ON;
-    RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-    RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
-    RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL9;
-    if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
-        Error_Handler();
-    }
-    /** Initializes the CPU, AHB and APB busses clocks
-    */
-    RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK
-                                  | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
-    RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
-    RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-    RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
-    RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
-
-    if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK) {
-        Error_Handler();
-    }
-}
 
 static void MX_GPIO_Init(void) {
     GPIO_InitTypeDef GPIO_InitStruct = {0};
@@ -86,11 +56,6 @@ void StartDefaultTask(void *argument) {
     }
 }
 
-void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim) {
-    if (htim->Instance == TIM1) {
-        HAL_IncTick();
-    }
-}
 
 void Error_Handler(void) {
 }
