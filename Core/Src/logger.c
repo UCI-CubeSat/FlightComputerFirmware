3bcc08b329ee38583daf6e771cecee46c1ec114a
Akash Idnani <aidnanidev@gmail.com>
2020-03-12 16:51:56 -0700

    UART via interrupts

--- Core/Src/logger.c
+++ Core/Src/logger.c
@@ -2,14 +2,15 @@
 #include "logger.h"
 
 UART_HandleTypeDef huart1;
-DMA_HandleTypeDef hdma_usart1_tx;
 
 uint8_t buf[] = {1, 2, 3};
 
-void logger_init(void (*error_handler) (void)) {
+void logger_init(void (*error_handler)(void)) {
     __HAL_RCC_USART1_CLK_ENABLE();
     __HAL_RCC_GPIOA_CLK_ENABLE();
-    __HAL_RCC_DMA1_CLK_ENABLE();
+
+    HAL_NVIC_SetPriority(USART1_IRQn, 0, 0);
+    HAL_NVIC_EnableIRQ(USART1_IRQn);
 
     GPIO_InitTypeDef GPIO_InitStruct = {0};
 
@@ -23,19 +24,6 @@ void logger_init(void (*error_handler) (void)) {
     GPIO_InitStruct.Pull = GPIO_NOPULL;
     HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
-    hdma_usart1_tx.Instance = DMA1_Channel4;
-    hdma_usart1_tx.Init.Direction = DMA_MEMORY_TO_PERIPH;
-    hdma_usart1_tx.Init.PeriphInc = DMA_PINC_DISABLE;
-    hdma_usart1_tx.Init.MemInc = DMA_MINC_ENABLE;
-    hdma_usart1_tx.Init.PeriphDataAlignment = DMA_PDATAALIGN_BYTE;
-    hdma_usart1_tx.Init.MemDataAlignment = DMA_MDATAALIGN_BYTE;
-    hdma_usart1_tx.Init.Mode = DMA_NORMAL;
-    hdma_usart1_tx.Init.Priority = DMA_PRIORITY_HIGH;
-    if (HAL_DMA_Init(&hdma_usart1_tx) != HAL_OK)
-    {
-        error_handler();
-    }
-
 
     huart1.Instance = USART1;
     huart1.Init.BaudRate = 115200;
@@ -45,27 +33,18 @@ void logger_init(void (*error_handler) (void)) {
     huart1.Init.Mode = UART_MODE_TX_RX;
     huart1.Init.HwFlowCtl = UART_HWCONTROL_NONE;
     huart1.Init.OverSampling = UART_OVERSAMPLING_16;
-    if (HAL_UART_Init(&huart1) != HAL_OK)
-    {
+    if (HAL_UART_Init(&huart1) != HAL_OK) {
         error_handler();
     }
 
-    __HAL_LINKDMA(&huart1,hdmatx,hdma_usart1_tx);
-
-    /* DMA interrupt init */
-    /* DMA1_Channel4_IRQn interrupt configuration */
-    HAL_NVIC_SetPriority(DMA1_Channel4_IRQn, 0, 0);
-    HAL_NVIC_EnableIRQ(DMA1_Channel4_IRQn);
 }
 
-void DMA1_Channel4_IRQHandler(void) {
-    HAL_DMA_IRQHandler(&hdma_usart1_tx);
-    __HAL_LINKDMA(&huart1, hdmatx, hdma_usart1_tx);
+void USART1_IRQHandler(void) {
+    HAL_UART_IRQHandler(&huart1);
 }
 
 void send_packet(void *packet, uint16_t size) {
-    HAL_UART_Transmit_DMA(&huart1, (uint8_t*) buf, 2);
-//    HAL_UART_Transmit(&huart1, (uint8_t*) packet, size, 10);
+    HAL_UART_Transmit_IT(&huart1, buf, 3);
 }
 
 void logger_send_imu(uint16_t roll, uint16_t pitch, uint16_t yaw) {
