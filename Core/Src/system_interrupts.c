1c6601017c646879f3b2f627b4216babb32e2c00
Akash Idnani <aidnanidev@gmail.com>
2020-03-11 23:52:44 -0700

    interrupt file name

--- Core/Src/system_interrupts.c
+++ Core/Src/system_interrupts.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include "interrupts.h"
+#include "system_interrupts.h"
 #include "FreeRTOS.h"
 #include "task.h"
 
@@ -32,16 +32,5 @@ void DebugMon_Handler(void) {
 }
 
 void TIM1_UP_IRQHandler(void) {
-    /* USER CODE BEGIN TIM1_UP_IRQn 0 */
-
-    /* USER CODE END TIM1_UP_IRQn 0 */
     HAL_TIM_IRQHandler(&htim1);
-    /* USER CODE BEGIN TIM1_UP_IRQn 1 */
-
-    /* USER CODE END TIM1_UP_IRQn 1 */
 }
-
-/* USER CODE BEGIN 1 */
-
-/* USER CODE END 1 */
-/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
