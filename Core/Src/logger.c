9556ced6adecca1bee1032a0da52a011bd15b87b
Akash Idnani <aidnanidev@gmail.com>
2020-03-12 17:00:13 -0700

    refactoring

--- Core/Src/logger.c
+++ Core/Src/logger.c
@@ -5,24 +5,37 @@ UART_HandleTypeDef huart1;
 
 uint8_t buf[] = {1, 2, 3};
 
-void logger_init(void (*error_handler)(void)) {
-    __HAL_RCC_USART1_CLK_ENABLE();
-    __HAL_RCC_GPIOA_CLK_ENABLE();
+void USART1_IRQHandler(void) {
+    HAL_UART_IRQHandler(&huart1);
+}
 
-    HAL_NVIC_SetPriority(USART1_IRQn, 0, 0);
-    HAL_NVIC_EnableIRQ(USART1_IRQn);
-
+void tx_gpio_init() {
     GPIO_InitTypeDef GPIO_InitStruct = {0};
-
     GPIO_InitStruct.Pin = GPIO_PIN_9;
     GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
     GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
     HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+}
 
+void rx_gpio_init() {
+    GPIO_InitTypeDef GPIO_InitStruct = {0};
     GPIO_InitStruct.Pin = GPIO_PIN_10;
     GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
     GPIO_InitStruct.Pull = GPIO_NOPULL;
     HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+}
+
+void gpio_init() {
+    __HAL_RCC_GPIOA_CLK_ENABLE();
+    tx_gpio_init();
+    rx_gpio_init();
+}
+
+void uart_init(void (*error_handler)(void)) {
+    __HAL_RCC_USART1_CLK_ENABLE();
+
+    HAL_NVIC_SetPriority(USART1_IRQn, 0, 0);
+    HAL_NVIC_EnableIRQ(USART1_IRQn);
 
 
     huart1.Instance = USART1;
@@ -36,11 +49,11 @@ void logger_init(void (*error_handler)(void)) {
     if (HAL_UART_Init(&huart1) != HAL_OK) {
         error_handler();
     }
-
 }
 
-void USART1_IRQHandler(void) {
-    HAL_UART_IRQHandler(&huart1);
+void logger_init(void (*error_handler)(void)) {
+    gpio_init();
+    uart_init(error_handler);
 }
 
 void send_packet(void *packet, uint16_t size) {
