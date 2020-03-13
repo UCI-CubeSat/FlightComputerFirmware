944fbd1ad1aa0901df584f0d25bacacb329a8fc9
Akash Idnani <aidnanidev@gmail.com>
2020-03-12 20:45:44 -0700

    Removed LED

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -10,16 +10,12 @@ const osThreadAttr_t defaultTask_attributes = {
 };
 
 
-static void MX_GPIO_Init(void);
-
 void StartDefaultTask(void *argument);
 
 int main(void) {
     HAL_Init();
     SystemClock_Config();
 
-    MX_GPIO_Init();
-    HAL_GPIO_WritePin(LED_GPIO_Port, LED_Pin, GPIO_PIN_RESET);
     logger_init(Error_Handler);
 
     osKernelInitialize();
@@ -31,27 +27,6 @@ int main(void) {
     while (1) {}
 }
 
-
-static void MX_GPIO_Init(void) {
-    GPIO_InitTypeDef GPIO_InitStruct = {0};
-
-    /* GPIO Ports Clock Enable */
-    __HAL_RCC_GPIOC_CLK_ENABLE();
-    __HAL_RCC_GPIOD_CLK_ENABLE();
-    __HAL_RCC_GPIOA_CLK_ENABLE();
-
-    /*Configure GPIO pin Output Level */
-    HAL_GPIO_WritePin(LED_GPIO_Port, LED_Pin, GPIO_PIN_RESET);
-
-    /*Configure GPIO pin : LED_Pin */
-    GPIO_InitStruct.Pin = LED_Pin;
-    GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-    GPIO_InitStruct.Pull = GPIO_NOPULL;
-    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-    HAL_GPIO_Init(LED_GPIO_Port, &GPIO_InitStruct);
-
-}
-
 void StartDefaultTask(void *argument) {
     for (;;) {
         logger_send_imu(30, 40, 50);
@@ -61,6 +36,5 @@ void StartDefaultTask(void *argument) {
 
 
 void Error_Handler(void) {
-    HAL_GPIO_WritePin(LED_GPIO_Port, LED_Pin, GPIO_PIN_RESET);
 }
 
